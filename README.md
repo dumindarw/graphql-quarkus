3641709695f155be4981f93ce9f190a7e98bf815
Rajitha Wanninayake <rajitha.wanninayake@mcmedisoft.com>
2020-03-03 15:42:23 +0530

    Added reactive MySQL integration

--- README.md
+++ README.md
@@ -2,20 +2,18 @@
 
 # GraphQL API
 
-query($uncompletedOnly: Boolean) {
-  allTasks(uncompletedOnly: $uncompletedOnly) {
+query {
+  allGroups {
     id,
-    description
+    name
   }
 }
 
 mutation{
-  complete(id:"4a9f53fd-584f-4169-b725-6b320043db8b")
+  addGroup(name:"TestGroup",createdBy:"Duminda")
 }
 
 This project uses Quarkus, the Supersonic Subatomic Java Framework.
-
-If you want to learn more about Quarkus, please visit its website: https://quarkus.io/ .
 
 ## Running the application in dev mode
 
@@ -39,5 +37,3 @@ You can create a native executable using: `./mvnw package -Pnative`.
 Or you can use Docker to build the native executable using: `./mvnw package -Pnative -Dquarkus.native.container-build=true`.
 
 You can then execute your binary: `./target/quarkus-graphql-1.0.0-SNAPSHOT-runner`
-
-If you want to learn more about building native executables, please consult https://quarkus.io/guides/building-native-image-guide .
--- src/main/java/org/drw/route/GroupResource.java
+++ src/main/java/org/drw/route/GroupResource.java
@@ -1,6 +1,7 @@
 package org.drw.route;
 
 import graphql.GraphQL;
+import graphql.scalars.ExtendedScalars;
 import graphql.schema.GraphQLSchema;
 import graphql.schema.idl.RuntimeWiring;
 import graphql.schema.idl.SchemaGenerator;
@@ -9,27 +10,22 @@ import graphql.schema.idl.TypeDefinitionRegistry;
 import io.vertx.core.Vertx;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.graphql.GraphQLHandler;
-import org.drw.model.Task;
-import org.drw.repo.TaskService;
+import org.drw.repo.GroupService;
 
 import javax.enterprise.event.Observes;
 import javax.inject.Inject;
 import javax.ws.rs.Path;
-import java.util.Map;
 
 @Path("/")
-public class TaskResource {
+public class GroupResource {
 
     @Inject
     Vertx vertx;
 
     @Inject
-    TaskService taskRepo;
-
-    private Map<String, Task> tasks;
+    GroupService taskRepo;
 
     public void init(@Observes Router router) {
-
 
         GraphQL graphQL = setupGraphQL();
         GraphQLHandler graphQLHandler = GraphQLHandler.create(graphQL);
@@ -39,14 +35,15 @@ public class TaskResource {
 
     private GraphQL setupGraphQL(){
 
-        String schema = vertx.fileSystem().readFileBlocking("tasks.graphqls").toString();
+        String schema = vertx.fileSystem().readFileBlocking("group.graphqls").toString();
 
         SchemaParser schemaParser = new SchemaParser();
         TypeDefinitionRegistry typeDefinitionRegistry = schemaParser.parse(schema);
 
         RuntimeWiring runtimeWiring = RuntimeWiring.newRuntimeWiring()
-                .type("Query", builder-> builder.dataFetcher("allTasks", taskRepo::allTasks))
-                .type("Mutation",builder -> builder.dataFetcher("complete", taskRepo::complete))
+                .type("Query", builder-> builder.dataFetcher("allGroups", taskRepo::allAvailableGroups))
+                .type("Mutation",builder -> builder.dataFetcher("addGroup", taskRepo::addGroup))
+                .scalar(ExtendedScalars.Date)
                 .build();
 
         SchemaGenerator schemaGenerator = new SchemaGenerator();
